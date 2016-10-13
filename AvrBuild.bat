3d5476be1003eaa72f76b0856ad4688b6e7934e4
Annie Zhang <annie.zhang@smartsparrow.com>
2016-10-13 16:13:43 +1100

    Added fresh project files

---
+++ AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\Annie\Desktop\Microwave\labels.tmp" -fI -W+ie -C V3 -o "C:\Users\Annie\Desktop\Microwave\Microwave.hex" -d "C:\Users\Annie\Desktop\Microwave\Microwave.obj" -e "C:\Users\Annie\Desktop\Microwave\Microwave.eep" -m "C:\Users\Annie\Desktop\Microwave\Microwave.map" "C:\Users\Annie\Desktop\Microwave\Microwave.asm"
